817281b4d2e74572936166dcde6b8c60a0325afc
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-10-10 19:51:41 +0200

    api inscription et connexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,24 +1,44 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinform = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials() {
-    // Ici, il faudrat appeler l'API pour vérifier les credentials en BDD
+    let dataForm= new FormData(signinform);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){ 
-        // Il faudra récupérere le vrai token
-        const token = "erkazehrmklazehrklazem23165earaz";
-        setToken(token);
-        // Placer ce token en cookie
-        
-        setCookie(RoleCookieName, "admin", 7);
-        window.location.replace("/");
+    let raw = JSON.stringify({
+    "username": dataForm.get("email"),
+    "password": dataForm.get("mdp"),
+    });
 
-    }else{
+    let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+};
+
+fetch(apiUrl+"login", requestOptions)
+  .then(response => {
+    if (response.ok) {
+        return response.json()
+     }
+     else{
         mailInput.classList.add("is-invalid");
         passwordInput.classList.add("is-invalid");
-        alert("Les identifiants sont incorrects.");
-    }
+     }
+  })
+  .then(result => {
+    const token = result.apiToken;
+    setToken(token);
+    // Placer ce token en cookie
+    
+    setCookie(RoleCookieName, result.roles[0], 7);
+    window.location.replace("/");
+  })
+  .catch(error => console.error("error", error));
 }
